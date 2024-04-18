6811b2cdd397ca7409c39690859e5220a28c791c
Camellia <camelliacgm@gmail.com>
2024-04-18 09:55:59 +0800

    delete the other person's codes to see if genarating conflicts.
    Signed-off-by: Camellia <camelliacgm@gmail.com>

--- __main__.py
+++ __main__.py
@@ -5,7 +5,6 @@ Created on Thu Apr 18 09:19:52 2024
 @author: xby
 """
 
-# this is modified by Leelu, to test the other person's pull.
 
 # and this is modified by Camellia, i will delete my first edit to see how merge
 # conflict works and test the other person's pull.
