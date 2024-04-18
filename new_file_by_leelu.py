f6eef6913e70d16423df1ac1b4dd97f5539be420
Leelu <leelucgm@gmail.com>
2024-04-18 10:17:50 +0800

    new file created by leelu
    Signed-off-by: Leelu <leelucgm@gmail.com>

---
+++ new_file_by_leelu.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 10:15:14 2024
+
+@author: Administrator
+"""
+
+# what if i add a new file in the repo, and commit to dev branch and 
+# then merge to the master branch, can the other person merge the 
+# remote master branch?
